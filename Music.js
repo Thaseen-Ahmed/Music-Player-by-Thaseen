ab41af46d2312c44c91cdf0c4fe2534a8ffbc2c8
ThaseenPS <thaseenahmedps235@gmail.com>
2023-07-07 11:04:21 +0530

    chnages

--- Music.js
+++ Music.js
@@ -1,3 +1,5 @@
+
+
 let now_playing = document.querySelector('.now-playing');
 let track_art = document.querySelector('.track-art');
 let track_name = document.querySelector('.track-name');
@@ -20,36 +22,38 @@ let isPlaying = false;
 let isRandom = false;
 let updateTimer;
 
+
 const music_list = [
     {
         img: 'images/bloody-sweet.webp',
         name: 'Bloody-sweet',
         artist: 'Anirudh',
-        music:  'musics/Bloody Sweet(PagalWorld.com.se).mp3'
+        music:  'musics/Bloody Sweet(PagalWorld.com.se).mp3',
 
     },
     {
         img: 'images/nee Singam-sweet.webp',
         name: 'Nee Singam Than',
         artist: 'Ar Rahman',
-        music:  'musics/Nee Singam Dhan Pathu Thala 128 Kbps.mp3'
+        music:  'musics/Nee Singam Dhan Pathu Thala 128 Kbps.mp3',
 
     },
     {
         img: 'images/images/pathan.jpg',
         name: 'Jhoome jo pathan',
         artist: 'Arjith singh',
-        music:  'musics/Jhoome Jo Pathaan(PagalWorld.com.se).mp3'
+        music:  'musics/Jhoome Jo Pathaan(PagalWorld.com.se).mp3',
 
     },
     {
         img: 'images/Tere Vaste.jpg',
         name: 'Tere Vaste',
         artist: 'Jubin Nautiyal',
-        music:  'musics/Tere Vaaste(PagalWorld.com.se).mp3'
+        music:  'musics/Tere Vaaste(PagalWorld.com.se).mp3',
 
     }
 ];
+
 loadTrack(track_index);
 
 function loadTrack(track_index){
@@ -64,7 +68,7 @@ function loadTrack(track_index){
     track_artist.textContent = music_list[track_index].artist;
     now_playing.textContent = "Playing music " + (track_index + 1) + " of " + music_list.length;
 
-    updateTimer = setInterval(setUpdate, 1500);
+    updateTimer = setInterval(setUpdate, 1000);
 
     curr_track.addEventListener('ended', nextTrack);
     random_bg_color();
@@ -172,144 +176,6 @@ function setUpdate(){
         if(durationMinutes < 10) { durationMinutes = "0" + durationMinutes; }
 
         curr_time.textContent = currentMinutes + ":" + currentSeconds;
-        total_duration.textContent = durationMinutes + ":" + durationMinutes;
+        total_duration.textContent = durationMinutes + ":" + durationSeconds;
     }
 }
-
-
-// let now_playing=document.querySelector('.now-playing');
-// let track_art=document.querySelector('.track-art');
-// let track_name=document.querySelector('track-name');
-// let track_artist=document.querySelector('track-artist');
-
-// let playpause_btn=document.querySelector('playpause-track');
-// let nxt_btn=document.querySelector('next-track');
-// let prev_btn=document.querySelector('prev-track');
-
-// let seek_slider=document.querySelector('seek-slider');
-// let volume_slider=document.querySelector('volume-slider');
-
-// let curr_time=document.querySelector('current-time');
-// let total_duration=document.querySelector('total-duration');
-// let wave=document.querySelector('wave');
-// let randomIcon=document.querySelector('.fa-random');
-// let curr_track=document.createElement('audio') 
-
-// let track_index=0;
-// let isPlaying=false;
-// let isRandom=false;
-// let updateTimer;
-
-
-// const music_list = [
-//     {
-//         img: 'images/bloody-sweet.webp',
-//         name: 'Bloody-sweet',
-//         artist: 'Anirudh',
-//         music:  'musics/Bloody Sweet(PagalWorld.com.se).mp3'
-
-//     },
-//     {
-//         img: 'images/nee Singam-sweet.webp',
-//         name: 'Nee Singam Than',
-//         artist: 'Ar Rahman',
-//         music:  'musics/Nee Singam Dhan Pathu Thala 128 Kbps.mp3'
-
-//     },
-//     {
-//         img: 'images/images/pathan.jpg',
-//         name: 'Jhoome jo pathan',
-//         artist: 'Arjith singh',
-//         music:  'musics/musics/Jhoome Jo Pathaan(PagalWorld.com.se).mp3'
-
-//     },
-//     {
-//         img: 'images/Tere Vaste.jpg',
-//         name: 'Tere Vaste',
-//         artist: 'Jubin Nautiyal',
-//         music:  'musics/musics/Tere Vaaste(PagalWorld.com.se).mp3'
-
-//     }
-// ];
-
-// loadTrack(track_index);
-// function loadTrack(track_index){
-//     clearInterval(updateTimer);
-//     reset();
-
-//     curr_track.src=music_list[track_index].music;
-//     curr_track.load();
-
-//     track_art.style.backgroundImage = "url("+ music_list[track_index].img + ")";
-//     track_name.textContent=music_list[track_index].name;
-//     track_artist.textContent=music_list[track_index].artist;
-//     now_playing.textContent = "Playing music " + (track_index+1) + "of" + music_list.length;
-
-//     updateTimer=setInterval(setUpdate,1500);
-
-//     curr_track.addEventListener('ended',nextTrack);
-//     random_bg_color();
-
-// }
-
-// function random_bg_color(){
-
-//     let hex=['0','1','2','3','4','5','6','7','8','a','b','c','d','e'];
-//     let a;
-
-//     function populate(a){
-//         for(let i=0;i<6;i++){
-//             let x= Math.round(math.random()*14);
-//             let y= hex[x];
-//             a+=y;
-//         }
-//         return a;
-//     }
-//     let Color1 = populate('#');
-//     let Color2 = populate('#');
-//     var angle = 'to right';
-
-//     let gradient= 'linear-gradient('+ angle + ',' + Color1 + ',' + Color2 +")";
-//     document.body.style.background = gradient;
-
-// }
-
-// function reset(){
-//     curr_time.textContent= "00:00";
-//     total_duration.textContent = "00:00";
-//     seek_slider.value=0;
-
-// }
-
-// function randomTrack(){
-//     isRandom ? pauseRandom() : playRandom();
-// }
-
-// function playRandom(){
-//     isRandom=true ;
-//     randomIcon.classList.add('randomActive');
-
-// }
-
-// function pauseRandom(){
-//     isRandom = false;
-//     randomIcon.classList.remove('randomActive');
-// }
-
-// function repeatTrack(){
-//     let current_index=track_index;
-//     loadTrack(current_index);
-//     playTrack();
-// }
-
-// function playpauseTrack(){
-//     isPlaying ? pauseTrack() : playTrack()
-// }
-
-// function playTrack(){
-//     curr_track.play();
-//     isPlaying=true;
-//     track_art.classList.add('rotate');
-//     wave.classList.add('loader');
-//     playpause_btn.innerHTML='<i class=fa fa-pause-circle fa-5x></i>';
-// }
